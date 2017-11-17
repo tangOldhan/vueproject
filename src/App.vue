afcc6a80967fb8267da7a07257183a2fc8a0430c
tangleileo <tanglei1007@gmail.com>
2017-11-17 16:58:35 +0800

    新增功能，进入页面在购物车徽章显示 购物车商品数量

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <header class="mint-header is-fixed"><div class="mint-header-button is-left"></div> <h1 class="mint-header-title">固定在顶部</h1> <div class="mint-header-button is-right"></div></header>
+    <header class="mint-header is-fixed">
+			<div class="mint-header-button is-left" v-text="showBack?'Back':''" @click="backto"></div> 
+			<h1 class="mint-header-title">vue project</h1> 
+			<div class="mint-header-button is-right"></div>
+		</header>
     <router-view class="main"></router-view>
     <nav class="mui-bar mui-bar-tab">
 			<router-link class="mui-tab-item" to="/home">
@@ -13,7 +17,7 @@
 			</router-link>
 			<router-link class="mui-tab-item" to="/shopcar">
 				<span class="mui-icon iconfont icon-cart">
-					<span id="badge" class="mui-badge">0</span>
+					<span id="badge" class="mui-badge" v-text="goodsquantity"></span>
 				</span>
 				<span class="mui-tab-label">购物车</span>
 			</router-link>
@@ -26,15 +30,47 @@
 </template>
 
 <script>
+import localdata from "./assets/js/localdata"
 import {vm,COUNTSTR} from "../src/assets/js/vm"
 export default {
 	name: 'app',
 	created(){
-		vm.$on(COUNTSTR,function(count){
-			var badge = document.querySelector("#badge");
-			badge.innerText = (badge.innerText - 0)+count;
+		this.getQuantity()
+		vm.$on(COUNTSTR,(count)=>{
+			console.log(count)
+			this.goodsquantity += count;
+			console.log('add one')
 		})
+		
 	},
+	data(){
+		return {
+			showBack:false,
+			goodsquantity:0
+		}
+	},
+	methods:{
+		backto(){
+			this.$router.go(-1)
+		},
+		getQuantity(){
+			var localcount=0;
+			var arr = localdata.getLocalData();
+			for(var i=0; i<arr.length; i++){
+				localcount += arr[i].count
+			}
+			this.goodsquantity = localcount
+		}
+	},
+	watch:{
+		"$route":function(newer,older){
+			if(newer.path !== "/home"){
+				this.showBack = true;
+			}else{
+				this.showBack = false;
+			}
+		}
+	}
 }
 </script>
 
